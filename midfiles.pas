77de7607fddc2176d904f6a24afc907ecd14995b
GSoftwareDevelopment <ghostsoftwaredevelopment@gmail.com>
2022-04-26 13:41:02 +0200

    correct chunks load

--- midfiles.pas
+++ midfiles.pas
@@ -1,31 +1,25 @@
 unit MIDFiles;
 
 interface
-const
-  MIDTRACKSREC_ADDR = $5F00;
-  MIDDATA_ADDR = $6000;
-
 type
+  TByteArray = Array[0..0] of byte;
+
+  PMIDTrack = ^TMIDTrack;
   TMIDTrack = record
     ptr:Pointer;
     deltaTime:Longint;
   end;
 
-  TMIDHeader = record
-    format:word;
-    nTracks:word;
-    fps:byte;
-    fsd:byte;
-    timing:word;
-  end;
-
 var
-  MID_DATA:Array[0..0] of byte absolute MIDDATA_ADDR;
-  Tracks:Array[0..0] of byte absolute MIDTRACKSREC_ADDR;
-  CurTrack:TMIDTrack;
-  MIDHeader:TMIDHeader;
+  MIDData:TByteArray;
+  MIDTracks:TByteArray;
+  format:word;
+  nTracks:word;
+  fps:byte;
+  fsd:byte;
+  timing:word;
 
-procedure LoadMIDI(fn:String);
+Procedure LoadMID(fn:String);
 
 implementation
 {$IFDEF DEBUG}
@@ -61,52 +55,73 @@ begin
   end;
 end;
 
-procedure LoadMIDI(fn:String);
+//
+//
+//
+
+procedure LoadMID(fn:String);
 var
   f:File;
-  Ofs:^Byte;
-  curTrack:byte;
-  trackCount:byte;
+  trackCount:word;
   chunkHead:String[4];
   v,dataPos:word;
   Len:Longint;
+  nTrkRec:PMIDTrack;
 
 begin
-  Ofs:=@MID_DATA;
+  nTrkRec:=@MIDTracks;
 {$IFDEF DEBUG}
   WriteLn('Open file ',fn);
 {$ENDIF}
   Assign(f,fn); Reset(f,1);
-  curTrack:=0; trackCount:=255; dataPos:=0;
+  trackCount:=0; dataPos:=0; nTracks:=255;
   SetLength(chunkHead,4);
-  while (not EOF(F)) and (curTrack<=trackCount) do
+  while (not EOF(F)) and (trackCount<nTracks) do
   begin
     BlockRead(f,chunkHead[1],4,v);
     if (v<>4) then break;
     Len:=ReadLongBI(f);
-{$IFDEF DEBUG}
-  WriteLn('Chunk type: ',ChunkHead);
-  WriteLn('Chunk size: ',Len);
-{$ENDIF}
+{$IFDEF DEBUG} Write('-',ChunkHead,'(',Len,') ');{$ENDIF}
     if chunkHead='MThd' then
     begin
-      MIDHeader.format:=readWordBI(f);
-      MIDHeader.nTracks:=readWordBI(f);
+      format:=readWordBI(f);
+      nTracks:=readWordBI(f);
       v:=readWordBI(f);
+{$IFDEF DEBUG}
+      WriteLn('Format: ',format);
+      WriteLn('Tracks: ',nTracks);
+{$ENDIF}
       if (v and $8000)<>0 then
       begin
-        MIDHeader.fps:=(v shr 8) and $7F;
-        case MIDHeader.fps of
-          $E8 : MIDHeader.fps:=24;
-          $E7 : MIDHeader.fps:=25;
-          $E3 : MIDHeader.fps:=29;
-          $E2 : MIDHeader.fps:=30;
+        fps:=(v shr 8) and $7F;
+        case fps of
+          $E8 : fps:=24;
+          $E7 : fps:=25;
+          $E3 : fps:=29;
+          $E2 : fps:=30;
         end;
+{$IFDEF DEBUG}
+        WriteLn('FPS: ',FPS);
+{$ENDIF}
+      end
+      else
+      begin
+{$IFDEF DEBUG}
+        WriteLn('TickDiv: ',v);
+{$ENDIF}
       end;
     end
     else if chunkHead='MTrk' then
     begin
-      BlockRead(f,MID_DATA[dataPos],Len);
+      inc(trackCount);
+{$IFDEF DEBUG}
+      WriteLn('Track: ',trackCount,'/',nTracks);
+      WriteLn('Size: ',Len);
+{$ENDIF}
+      BlockRead(f,MIDData[dataPos],Len);
+      nTrkRec^.ptr:=@MIDData[dataPos];
+      nTrkRec^.deltaTime:=0;
+      inc(nTrkRec,1);
       inc(dataPos,Len);
     end;
   end;
