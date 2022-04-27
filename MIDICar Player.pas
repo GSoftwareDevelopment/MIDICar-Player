6e531e03bc0e3b1e9b99dd40ff4f36c69d4fd0d5
GSoftwareDevelopment <ghostsoftwaredevelopment@gmail.com>
2022-04-27 22:51:47 +0200

    Added loop that plays one channel of a MID file. No time synchronisation.

--- MIDICar Player.pas
+++ MIDICar Player.pas
@@ -1,4 +1,5 @@
 Uses MC6850,MIDI_FIFO,MIDFiles;
+{$I-}
 
 Procedure MIDI_SendNoteOn(ch,note,vel:byte);
 begin
@@ -10,15 +11,38 @@ begin
   FIFO_WriteByte(ZP_Data);
 end;
 
+var
+  fn:string;
+  Track:PMIDTrack;
+  delta:Longint;
+
 begin
   FIFO_Reset;
   MC6850_Reset;
   MC6850_Init(CD_64+WS_OddParity+WS_8bits);
 
+  if paramCount=1 then
+    fn:=ParamStr(1)
+  else
+    fn:='D2:SELFTEST.MID';
+
   MIDData:=Pointer($6000);
   MIDTracks:=Pointer($5F00);
-  LoadMID('D2:OVERWORL.MID');
+  LoadMID(fn);
 
+  Repeat
+    Track:=@MIDTracks[0];
+    delta:=Track^.DeltaTime;
+    // WriteLn(delta);
+    if delta=0 then
+      GetTrackData(Track)
+    else
+    begin
+      dec(delta);
+      Track^.DeltaTime:=delta;
+    end;
+    FIFO_Flush;
+  until Track^.EOT;
 //  MIDI_SendNoteOn(0,64,64);
 //  FIFO_Flush;
 end.
