30d80f429d6e4387c32418a8f896650aa41f4919
GSoftwareDevelopment <ghostsoftwaredevelopment@gmail.com>
2022-05-18 18:26:46 +0200

    update

--- MIDICar Player.pas
+++ MIDICar Player.pas
@@ -1,73 +1,125 @@
+{$LIBRARYPATH midfile/}
 {$DEFINE ROMOFF}
 Uses
   MC6850,
   MIDI_FIFO,
-  MIDFiles;
+  MIDFiles,
+  sysutils,
+  Misc,
+  CIO;
 {$I-}
 
 const
-  TRACK_DATA_ADDR = $4000;
-  MIDI_DATA_ADDR  = $4100;
+  DLIST_ADDR      = $3400;
+  SCREEN_ADDR     = $3430;
+  SCREEN_STATUS   = SCREEN_ADDR+25*40;
+  TRACK_DATA_ADDR = $3E00;
+  MIDI_DATA_ADDR  = $4000;
+  FREE_MEM        = (($8000-$4000)+($d000-$a000)+($ff00-$e000)) div 1024;
 
 {$r selftest.rc}
 
-const
-  GM_RESET: array[0..5] of byte = ($f0, $7e, $7f, $09, $01, $f7);
-
 var
-  fn:PString;
-  trkPtr:PMIDTrack;
-  curTrackOfs:Byte;
-  deltaTime:TDeltaTime;
-  dTm:word;
-  cTrk,PlayingTracks,v:Byte;
-
+  chptr:array[0..39] of word;
+  scradr:Word;
 
 // {$I mc6850_irq.inc}
+{$i helpers.inc}
 
-procedure reset_MIDI;
+procedure clearStatus;
 begin
-  FIFO_Send(GM_Reset,6); FIFO_Flush;
+  fillchar(pointer(SCREEN_STATUS),40,128);
 end;
 
-begin
-{$IFDEF DEBUG}
-  WriteLn('Debug mode is On');
-{$ENDIF}
-{$IFDEF USE_FIFO}
-  // WriteLn('FIFO: On');
-  FIFO_Reset;
-{$ELSE}
-  WriteLn('FIFO: Off');
-{$ENDIF}
+procedure init;
+var
+  totalXMS:word;
+  x,y:byte;
 
-  WriteLn('MID Player for MIDICar RC1 2022 GSD');
+begin
+{$IFDEF USE_FIFO}
+  FIFO_Reset;
+{$ENDIF}
+  // copy charset definition
+  move(pointer($e000),pointer($3000),$400);
+  poke(709,15); poke(710,0);
+  poke($2F4,$30);
+  fillchar(pointer(SCREEN_ADDR),1040,0);
+  dpoke($230,DLIST_ADDR);
+
+  fillchar(pointer(SCREEN_ADDR),40,128);
+  scradr:=SCREEN_ADDR+1; PutText('MIDICar Player RC1'~*);
+  scradr:=SCREEN_ADDR+31; PutText('2022 GSD'~*);
+  clearStatus;
+
+  // detect memory
+  totalXMS:=DetectMem*16;
+  asm lda #$fe \ sta $100 end;
+  // writeLn('Available memory: ',FREE_MEM+totalXMS,' KB');
+
   MIDTracks:=Pointer(TRACK_DATA_ADDR);
   MIDData:=Pointer(MIDI_DATA_ADDR);
 
+  for cTrk:=0 to 39 do
+  begin
+    x:=cTrk div 20; y:=cTrk mod 20;
+    chPtr[cTrk]:=SCREEN_ADDR+80+80+x*20+y*40;
+  end;
+end;
+
+procedure loadPrc(v:byte);
+var
+  i:byte;
+
+begin
+  scradr:=SCREEN_STATUS+10;
+  putInt(v-1); poke(scradr,$8f); inc(scradr);
+  putInt(totalTracks);
+end;
+
+procedure checkParams;
+var
+  fn:PString;
+  err:shortint;
+
+begin
   if paramCount=1 then
   begin
     fn:=ParamStr(1);
-    v:=LoadMid(fn);
-    write(#155);
-    if v<>0 then
+    scradr:=SCREEN_STATUS+1; PutText('Loading ../..'~*);
+    puttextinvert:=128;
+    loadProcess:=@loadPrc;
+    err:=LoadMid(fn);
+    if err<>0 then
     begin
-      case v of
-        ERR_UNSUPPORTED_FORMAT: writeLn('Unsupported format');
-        ERR_NOT_ENOUGHT_MEMORY: writeLn('Not enought memory');
-        128..255: WriteLn('I/O Error #',v);
+      scradr:=SCREEN_STATUS+1;
+      if err>0 then
+      case err of
+        ERR_UNSUPPORTED_FORMAT: PutText('Unsupported format'~*);
+        ERR_NOT_ENOUGHT_MEMORY: PutText('Not enought memory'~*);
+      end
+      else
+      begin
+        PutText('I/O Error #'~*); putInt(err);
       end;
-      halt;
+      while keyb=255 do ;
+      keyb:=255;
+      exit2DOS;
     end;
   end
   else
   begin
-    writeLn('Type "P device:filename.MID" to play file.');
-    nTracks:=1;
-    writeLn('Selftest...');
+    totalTracks:=1;
   end;
+end;
+
+begin
+  init;
+  checkParams;
 
 //
+  clearStatus;
+  scradr:=SCREEN_STATUS+1; PutText('Playing'~*);
 
   // initialize MIDICar (MC6850)
   MC6850_Reset;
@@ -76,65 +128,45 @@ begin
   // initialize IRQ for MC6850
   // oldIRQ:=VIMIRQ; asm SEI end; VIMIRQ:=@MC6850_IRQ; asm CLI end;
 
-  Write('Playing...');
-
   Reset_MIDI;        // reset MIDI
   _totalTicks:=0;    // reset song ticks
-  // setIntVec(iTim1,@int_play,0,255);
   setTempo;          // set song tempo
 
 // Player loop
 
-  cTrk:=nTracks;
+  cTrk:=totalTracks;
   Repeat
 
-    if cTrk=nTracks then
-    begin
-      curTrackOfs:=0; cTrk:=1;
-      PlayingTracks:=nTracks;
-    end
-    else
-    begin
-      inc(curTrackOfs,sizeOf(TMIDTrack));
-      inc(cTrk);
-    end;
+    processMIDI;
 
-    move(@MIDTracks[curTrackOfs],pointer(_trkRegs),sizeOf(TMIDTrack)); // copy current track data
-
-    if _trackTime>=0 then
-    begin
-      if _totalTicks>=_trackTime then
-      begin
-        _timerStatus:=_timerStatus or f_counter;     // pause ticking
-        {$IFDEF DEBUG} poke($d01a,15); {$ENDIF}
-        dTm:=_totalTicks-_trackTime;                 // time correction
-        deltaTime:=ProcessTrack;
-        if deltaTime>0 then
-          _trackTime:=_totalTicks+deltaTime-dTm      // calculate new track time with time correction
-        else
-          _trackTime:=-1;
-        {$IFDEF DEBUG} poke($d01a,0); {$ENDIF}
-        _timerStatus:=_timerStatus and (not f_counter);  // resume ticking
-      end;
-    end
-    else
-      Dec(PlayingTracks);
-    move(pointer(_trkRegs),@MIDTracks[curTrackOfs],sizeOf(TMIDTrack)); // copy current track data
+    // scradr:=chptr[cTrk];
+    // PutByteHex(_bank); inc(scradr,3);
+    // PutWordHex(_adr); inc(scradr,6);
+    // PutLongHex(_trackTime); // _delta
 
 {$IFDEF USE_FIFO}
-    if (MC6850_CNTRReg and TDRE)<>0 then
-      if FIFO_Tail<>FIFO_Head then
-      begin
-{$IFDEF DEBUG} poke($d01a,4); {$ENDIF}
-        FIFO_ReadByte;
-        MC6850_BUFFER:=FIFO_Byte;
-{$IFDEF DEBUG} poke($d01a,0); {$ENDIF}
-      end;
+    FIFO_PushDirect2MC6850;
 {$ENDIF}
 
-  until (PlayingTracks=0) or (peek(764)<>255);
+    if keyb<>255 then
+    begin
+      case keyb of
+        k_esc: break;
+        k_p:
+          begin
+            _timerStatus:=_timerStatus xor f_counter;
+            scradr:=SCREEN_STATUS+1;
+            if _timerStatus and f_counter=0 then
+              PutText('Playing'~*)
+            else
+              PutText('Pause  '~*);
+          end;
+      end;
+      keyb:=255;
+    end;
+  until (PlayingTracks=0);
 
-  Write(#156);
+  // Write(#156);
 
 // End player loop
 
@@ -148,4 +180,5 @@ begin
 
   // resotre Timer vector
   setIntVec(iTim1,oldTimerVec);
+  exit2DOS;
 end.
