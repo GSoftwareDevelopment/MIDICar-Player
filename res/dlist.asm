b7fd3c44bf89e81134aebb429f7b89cbc8489a9b
GSoftwareDevelopment <ghostsoftwaredevelopment@gmail.com>
2022-12-08 09:41:18 +0100

    change display list for horizontal meter

--- res/dlist.asm
+++ res/dlist.asm
@@ -1,23 +1,36 @@
     icl 'dlist.inc'
     icl '../memory.inc'
+title_bar   = DL_MODE_320x192G2
+work_area   = DL_MODE_40x24T2
+vis_area    = DL_MODE_20X24T5
+stat_area   = DL_MODE_20X12T5
+prgs_area   = DL_MODE_160x96G4
+foot_area   = DL_MODE_40x24T2
 
 dl_start
     :2  dta DL_BLANK6
 
-        dta DL_MODE_320x192G2 + DL_LMS, A(SCREEN_HEAD)
-    :18 dta DL_MODE_320x192G2
+        dta title_bar + DL_LMS, A(SCREEN_HEAD)
+    :18 dta title_bar
 
         dta DL_BLANK1
 
-        dta DL_MODE_40x24T2 + DL_LMS, A(SCREEN_WORK), DL_BLANK1
-    :17 dta DL_MODE_40x24T2, DL_BLANK1
+        dta work_area + DL_LMS, A(SCREEN_WORK), DL_BLANK1
+    :11 dta work_area, DL_BLANK1
 
-        dta DL_MODE_20x12T5 + DL_LMS, A(SCREEN_TIME)
-        dta DL_MODE_160x96G4 + DL_LMS, A(SCREEN_TIMELINE)
-        dta DL_BLANK1
-        dta DL_MODE_40x24T2 + DL_LMS, A(SCREEN_TIME + 20)
+        dta work_area + DL_LMS, A(SCREEN_CHANNELS)
+    :4  dta vis_area
 
         dta DL_BLANK1
-        dta DL_MODE_40x24T2 + DL_LMS, A(SCREEN_FOOT)
+        dta work_area
+        dta DL_BLANK1
+
+        dta stat_area + DL_LMS, A(SCREEN_TIME)
+        dta prgs_area + DL_LMS, A(SCREEN_TIMELINE)
+        dta DL_BLANK1
+        dta work_area + DL_LMS, A(SCREEN_TIME + 20)
+
+        dta DL_BLANK1
+        dta foot_area + DL_LMS, A(SCREEN_FOOT)
 
         dta DL_JVB, A(dl_start)
