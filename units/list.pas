02b855f9fac189aa7204128253a839ce724085fb
GSoftwareDevelopment <ghostsoftwaredevelopment@gmail.com>
2023-06-04 20:00:14 +0200

    function unification & update; rename a65 file

--- units/list.pas
+++ units/list.pas
@@ -2,48 +2,102 @@ unit list;
 
 interface
 
-var
-  fileList:Pointer absolute $DA;
-  p_bank:byte;  //  \
-  p_adr:word;   //  / It is important that this order remains intact
+const
+  ERR_LIST_FOUND     = 96;
+  ERR_LIST_ENTRY_END = 97;
+  ERR_LIST_END       = 98;
+  ERR_LIST_IS_FULL   = 99;
 
-function getEntry(fn:PString):Boolean; register; assembler;
-procedure getEntrySets(var ptr); register; assembler;
-procedure setEntrySets(var ptr); register; assembler;
-function nextEntry:Boolean; assembler;
-procedure gotoNEntry(nEntry:SmallInt); assembler;
-procedure addToList(entry:PString); Register; Assembler;
+var
+  listPtr:Pointer absolute $DA;
+  p_type:byte;
+
+{
+Fetch string to `fn` from current pointer `listPtr`.
+
+After this operation, check IOResult!
+The lack of change in IOResult, shows that the end of the record has not been reached!
+}
+procedure list_getText(fn:PString); register; assembler;
+
+{
+Adds a new entry to the list at the location indicated by the `listPtr` pointer.
+
+list_putText  writes a string ending in an inverted
+              it's also an indication of the end of the record!
+list_putNText writes a null-terminated string
+              this does not end the record.
+
+The pointer is incremented by the length of the string (+1 when null-terminated).
+Returns an error when it reaches the list limit.
+}
+procedure list_putText(entry:PString); Register; Assembler;
+procedure list_putNText(entry:PString); Register; Assembler;
+
+{
+It retrieves one byte from the location that `listPtr` points to.
+
+If the fetched byte is set to the 7th bit, it will be treated as the
+end of the record (ERR_LIST_ENTRY_END error).
+When it is equal to $9B (Atari EOL) an ERR_LIST_END error will be returned.
+}
+procedure list_getByte(var ptr); register; assembler;
+
+{
+Put one byte in current location that `listPtr` points to.
+
+The pointer is incremented by 1 byte.
+Returns an error when it reaches the list limit.
+}
+procedure list_putByte(var ptr); register; assembler;
+
+{
+Moves the `listPtr` pointer to the next record.
+
+The operation can be performed when the end of the record has not been reached.
+}
+procedure list_nextEntry; assembler; keep;
+
+{
+Sets the `listPtr` pointer to the nth element of the list.
+}
+procedure list_go2Entry(nEntry:SmallInt); assembler;
 
 implementation
 
-function getEntry(fn:PString):Boolean; register; assembler;
+procedure list_getText(fn:PString); register; assembler;
 asm
-  icl 'asms/list/getEntry.a65'
+  icl 'asms/list/getText.a65'
 end;
 
-procedure getEntrySets(var ptr); register; assembler;
+procedure list_putText(entry:PString); Register; Assembler;
 asm
-  icl 'asms/list/getEntrySets.a65'
+  icl 'asms/list/putText.a65'
 end;
 
-procedure setEntrySets(var ptr); register; assembler;
+procedure list_putNText(entry:PString); Register; Assembler;
 asm
-  icl 'asms/list/setEntrySets.a65'
+  icl 'asms/list/putNText.a65'
 end;
 
-function nextEntry:Boolean; assembler;
+procedure list_getByte(var ptr); register; assembler;
+asm
+  icl 'asms/list/getSets.a65'
+end;
+
+procedure list_putByte(var ptr); register; assembler;
+asm
+  icl 'asms/list/putSets.a65'
+end;
+
+procedure list_nextEntry; assembler; keep;
 asm
   icl 'asms/list/nextEntry.a65'
 end;
 
-procedure gotoNEntry(nEntry:SmallInt); assembler;
+procedure list_go2Entry(nEntry:SmallInt); assembler;
 asm
-  icl 'asms/list/gotoNEntry.a65'
-end;
-
-procedure addToList(entry:PString); Register; Assembler;
-asm
-  icl 'asms/list/addToList.a65'
+  icl 'asms/list/gotoEntry.a65'
 end;
 
 end.
